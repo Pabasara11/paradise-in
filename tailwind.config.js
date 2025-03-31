683237c65fe32551d4fcf46d8f8b140655fb6053
Pabasara Rajapaksha <piumipabasara2001@gmail.com>
2025-04-01 00:10:14 +0530

    create header component and resolved the tailwindcss errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
- /** @type {import('tailwindcss').Config} */
- export default {
-    content: [
-        "./index.html",
-        "./src/**/*.{html,js}"
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", // for the HTML files
+    "./src/**/*.{js,jsx,ts,tsx}", // for all React files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
